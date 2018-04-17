bae8e04827bc53019f4006b1d41addd9ea639c7d
nisevi <nicolas.s.vidal@gmail.com>
2018-04-17 12:03:20 +0200

    Test Matrix class 'initialize' method.

--- spec/lib/primes_table/matrix_spec.rb
+++ spec/lib/primes_table/matrix_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe 'Matrix.' do
-  xit 'should initialize a new matrix'
+  let(:args) { { rows: 10, columns: 10 } }
 
-  xit 'should print a matrix of primes'
+  it 'should initialize a new matrix' do
+    matrix = Matrix.new(args)
+    expect(matrix.rows_header).to eq([2, 3, 5, 7])
+    expect(matrix.columns_header).to eq([2, 3, 5, 7])
+    expect(matrix.table).to eq(
+      [
+        [4,  6,  10, 14],
+        [6,  9,  15, 21],
+        [10, 15, 25, 35],
+        [14, 21, 35, 49]
+      ]
+    )
+  end
 end
