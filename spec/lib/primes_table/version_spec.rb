e20eab00a3b121ee92b5fedca77bb4e370be3b69
nisevi <nicolas.s.vidal@gmail.com>
2018-04-24 11:19:39 +0200

    Version name convention it must be 0.1.2

--- spec/lib/primes_table/version_spec.rb
+++ spec/lib/primes_table/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'Check app version.' do
   it 'should be the latest version' do
-    expect(PrimesTable::VERSION).to match('0.1.0')
+    expect(PrimesTable::VERSION).to match('0.1.2')
   end
 end
